4bf65d71993c37eaeeefda05b36b68c6a21524b3
unknown <s223583@student.uek.krakow.pl>
2022-04-20 20:15:35 +0200

    2022-04-10 KK

--- README.md
+++ README.md
@@ -12,21 +12,21 @@
 |treść|div.user-post__text|content|str|
 |data wystawienia|span.user-post__published > time:nth-child(1)\[datetime\]|publish_date|str|
 |data zakupu|span.user-post__published > time:nth-child(2)\[datetime\]|purchase_date|str|
-|dla ilu przydatna|button.vote-yes > yes[date-total-vote] <br> 
+|dla ilu przydatna|button.vote-yes[date-total-vote] <br> 
 button.vote-yes > span <br>
 span[id^=votes-yes]|useful|int|
-|dla ilu nieprzydatna|button.vote-no > no[date-total-vote] <br> 
+|dla ilu nieprzydatna|button.vote-no[date-total-vote] <br> 
 button.vote-no > span <br>
 span[id^=votes-no]|useless|int|
-|lista zalet|div.review-feature__title--positives ~ review-feature__item <br>div.review-feature__col:has( > div.review-feature__title--positives) > review-feature__item
+|lista zalet|div.review-feature__title--positives ~ review-feature__item <br>div.review-feature__col:has( > div.review-feature__title--positives) > div.review-feature__item
 <br>
-div.review-feature__title--positives ~ div.review-feature__item|pros|str|
-|lista wad|div.review-feature__title--negatives ~ review-feature__item <br>div.review-feature__col:has( > div.review-feature__title--negatives) > review-feature__item
+div.review-feature__item:has( ~ div.review-feature__title--positives)|pros|str|
+|lista wad|div.review-feature__title--negatives ~ div.review-feature__item <br>div.review-feature__col:has( > div.review-feature__title--negatives) > div.review-feature__item
 <br>
-div.review-feature__title--negatives ~ div.review-feature__item|cons|str|
+div.review-feature__item:has( ~ div.review-feature__title--negatives)|cons|str|
 
 ## Etapy pracy nad projektem
 1) pobranie skladowych pojedyńczej opinii do niezależnych zmiennych 
 2) zapisanie wszystkich składowych pojedynczej opinii do obiektu słownika (dictionary)
 3) pobranie wszystkiech opinii z pojedynczej strony i zapisanie ich do listy słowników 
-4) 
+4) pobranie wszystkich opinii o wskazanym produkcie i zapisanie ich do pliku
--- scraper.py
+++ scraper.py
@@ -2,66 +2,86 @@ import json
 import requests
 from bs4 import BeautifulSoup
 
+def extract_element(ancestor, selector, attribute=None):
+    try:
+        if attribute:
+            return ancestor.select(selector).pop(0)[attribute].strip()
+        else:
+            return ancestor.select(selector).pop(0).text.strip()
+
+    except IndexError: return None
 
 
-url = "https://www.ceneo.pl/95365253#tab=reviews"
-response = requests.get(url)
-
-page_dom = BeautifulSoup(response.text, 'html.parser')
-
-reviews = page_dom.select("div.js_product-review")
+product_id = input("Podaj identyfikator produktu: ")
+url_pre = "https://www.ceneo.pl/"
+url_post = "/opinie-"
+page_no = 1
 
 all_reviews = []
+while(page_no):
+    url = url_pre+product_id+url_post+str(page_no)
+    response = requests.get(url, allow_redirects=False)
+    if response.status_code == requests.codes.ok: 
+        page_dom = BeautifulSoup(response.text, 'html.parser')
+        reviews = page_dom.select("div.js_product-review")
 
-for review in reviews: 
+        for review in reviews: 
 
-    review_id = review["data-entry-id"]
+            review_id = review["data-entry-id"]
 
-    author = review.select("span.user-post__author-name").pop(0).text.strip()
-    try:
-        recomendation = review.select("span.user-post__author-recomendation > em").pop(0).text
+            author = extract_element(review,"span.user-post__author-name")
+    
+            recommendation = extract_element(review,"span.user-post__author-recomendation > em")
 
-        recomendation = True if recomendation == "Polecam" else False
-    except: recomendation = None
-    stars = review.select("span.user-post__score-count").pop(0).text
-    stars = float(stars.split("/").pop(0).replace(",","."))
+            stars = extract_element(review,"span.user-post__score-count")
 
-    content = review.select("div.user-post__text").pop(0).get_text()
+            content = extract_element(review,"div.user-post__text")
 
-    content = content.replace("\n"," ").replace("  "," ")
+            publish_date = extract_element(review,"span.user-post__published > time:nth-child(1)", "datetime")
 
-    publish_date = review.select("span.user-post__published > time:nth-child(1)").pop(0)["datetime"]
+            purchase_date = extract_element(review,"span.user-post__published > time:nth-child(2)","datetime")
+          
+            useful = extract_element(review,"span[id^=votes-yes]")
+            
+            useless = extract_element(review,"span[id^=votes-no]")
 
-    try:
-        purchase_date = review.select("span.user-post__published > time:nth-child(2)").pop(0)["datetime"]
-    except IndexError: purchase_date = None
-    useful = review.select("span[id^=votes-yes]").pop(0).text
-    useful = int(useful)
-    useless = review.select("span[id^=votes-no]").pop(0).text
-    useless = int(useless)
+            pros = review.select("div.review-feature__title--positives ~ div.review-feature__item")
 
-    pros = review.select("div.review-feature__title--positives ~ div.review-feature__item")
+            pros = [item.text.strip() for item in pros]
+            pros = ", ".join(pros)
 
-    pros = [item.text.strip() for item in pros]
-    pros = ", ".join(pros)
-    cons = review.select("div.review-feature__title--negatives ~ div.review-feature__item")
+            cons = review.select("div.review-feature__title--negatives ~ div.review-feature__item")
 
-    cons = [item.text.strip() for item in cons]
-    cons = ", ".join(cons)
+            cons = [item.text.strip() for item in cons]
+            cons = ", ".join(cons)
 
-    single_review = {
-        "review_id": review_id,
-        "author": author,
-        "recomendation":recomendation,
-        "stars": stars,
-        "content": content,
-        "publish_date":publish_date,
-        "purchase_date":purchase_date,
-        "useful":useful,
-        "useless":useless,
-        "pros":pros,
-        "cons":cons
-    }
-    all_reviews.append(single_review)
+            recommendation = True if recommendation == "Polecam" else False if recommendation == "Nie polecam" else None
+            stars = float(stars.split("/").pop(0).replace(",", "."))
+            content = content.replace("\n", " ").replace("  ", " ").strip()
+            useful = int(useful)
+            useless = int(useless)
 
-    print(json.dumps(all_reviews,indent=4, ensure_ascii=False))
+            single_review = {
+                "review_id": review_id,
+                "author": author,
+                "recommendation":recommendation,
+                "stars": stars,
+                "content": content,
+                "publish_date":publish_date,
+                "purchase_date":purchase_date,
+                "useful":useful,
+                "useless":useless,
+                "pros":pros,
+                "cons":cons
+            }
+            all_reviews.append(single_review)
+        page_no+=1
+
+    else: page_no = None
+f = open("reviews/"+product_id+".json","w", encoding="UTF-8")
+
+json.dump(all_reviews, f,indent=4, ensure_ascii=False)
+print(len(all_reviews))
+
+
+
